080eb6a2c4e9d8330e278e9042d409d1407b91b7
Kamma Pradeep <142683667+Pradeepkamma@users.noreply.github.com>
2025-03-18 13:45:25 +0530

    Update CrudUsingMethods.java

--- Method3/CrudUsingMethods.java
+++ Method3/CrudUsingMethods.java
@@ -1,7 +1,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.SQLException
 
 public class CrudUsingMethods {
 	private static final String url = "jdbc:mysql://localhost:3306/jdbcdemo"; 
